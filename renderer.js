218fc4378b3f1f4ca31cb0f1b982d4ec45cfed88
Alex <alex@dzvo.org>
2023-05-28 18:13:03 +0200

    add track detection

--- renderer.js
+++ renderer.js
@@ -7,35 +7,8 @@ const LapList = require('./lib/components/LapList');
 const FileProcessor = require('./lib/FileProcessor');
 const FileDownloader = require('./lib/FileDownloader');
 
-const laps = [];
-const lapFiles = fs.readdirSync('dl/laps')
-  .filter((element) => element.endsWith('.json'));
-lapFiles.forEach((element) => {
-  const content = fs.readFileSync(`dl/laps/${element}`, { encoding: 'utf-8' });
-  const data = JSON.parse(content);
-  laps.push(data);
-  console.log(data.laptime);
-});
 
 const gpsChart = new GpsChart('gpsChart');
-
-const lapList = new LapList(
-  'lapList',
-  laps,
-  (index, color, trackname) => {
-    gpsChart.setTrack(trackname);
-    gpsChart.addElement(index, laps[index], color);
-    gpsChart.draw();
-    plot.addElement(index, laps[index], color);
-    plot.draw();
-  },
-  (index) => {
-    gpsChart.removeElement(index);
-    gpsChart.draw();
-    plot.removeElement(index);
-    plot.draw();
-  },
-);
 
 const plot = new ComparisonChart(
   'plot',
@@ -48,10 +21,47 @@ const plot = new ComparisonChart(
   },
 );
 
+var laps = [];
+var lapList = null;
+
+function populateLaps() {
+  laps = [];
+  const lapFiles = fs.readdirSync('dl/laps')
+    .filter((element) => element.endsWith('.json'));
+  lapFiles.forEach((element) => {
+    const content = fs.readFileSync(`dl/laps/${element}`, { encoding: 'utf-8' });
+    const data = JSON.parse(content);
+    laps.push(data);
+    console.log(
+      `%cfound lap of ${data.track} with laptime ${data.laptime} on ${data.samples[0].timestamp}`,
+      `color: gray;`
+    );
+  });
+
+  lapList = new LapList(
+    'lapList',
+    laps,
+    (index, color, trackname) => {
+      gpsChart.setTrack(trackname);
+      gpsChart.addElement(index, laps[index], color);
+      gpsChart.draw();
+      plot.addElement(index, laps[index], color);
+      plot.draw();
+    },
+    (index) => {
+      gpsChart.removeElement(index);
+      gpsChart.draw();
+      plot.removeElement(index);
+      plot.draw();
+    },
+  );
+}
+populateLaps();
+
 document.querySelector('#buttonload').addEventListener('click', () => {
   const fd = new FileDownloader();
   fd.downloadFiles().then(() => {
     const fp = new FileProcessor();
     fp.parseFiles();
-  }, (e) => { alert(e); });
+  }, (e) => { alert(e); }).finally(() => { populateLaps(); alert("Finished"); });
 });
